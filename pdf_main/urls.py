6d5386d16de90fb297cd4fc9a95059b036d744a3
GeonwooVincentKim <kdsnop@gmail.com>
2020-01-31 10:44:08 +0900

    Update Code

--- pdf_main/urls.py
+++ pdf_main/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     url(r'^csv_test/', views.test_view_csv),
     url(r'^csv_test_2/', views.test_view_csv_2),
     url(r'^csv_test_3/', views.test_streaming_csv_view),
-    url(r'^pdf_test/', views.pdf_some_view),
+    url(r'^pdf_test/', views.test_view_pdf),
+    url(r'^pdf_test_2/', views.test_view_pdf_2),
 ]
--- pdf_main/views.py
+++ pdf_main/views.py
@@ -1,5 +1,8 @@
 import csv
 
+# from django.utils.six import moves
+from io import BytesIO
+
 from django.http import HttpResponse, StreamingHttpResponse
 from django.shortcuts import render
 from django.template import loader, Context
@@ -64,11 +67,11 @@ def test_streaming_csv_view(request):
     return response
 
 
-def pdf_some_view(request):
+def test_view_pdf(request):
     response = HttpResponse(content_type='application/pdf')
     response['Content-Disposition'] \
         = 'attachment;' \
-          'filename="somefilename.pdf"'
+          'filename="pdf_test_1.pdf"'
 
     # Use the response object as "file"
     # to create the PDF object.
@@ -86,4 +89,31 @@ def pdf_some_view(request):
     # Closing the PDF object ends.
     p.showPage()
     p.save()
+    return response
+
+
+def test_view_pdf_2(request):
+    # Create a HttpResponse Object using appropriate PDF header.
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] \
+        = 'attachment;' \
+          'filename="pdf_test_2.pdf"'
+    buffer = BytesIO()
+
+    # Create a PDF object using BytesIO as "File"
+    p = canvas.Canvas(buffer)
+
+    # Draw PDF Contents.
+    # This is the position where PDF generate.
+    # See the ReportLab documentation for the full list of features.
+    p.drawString(100, 100, "Hello World.")
+
+    # Completely close the PDF object.
+    p.showPage()
+    p.save()
+
+    # Gets the value of the BytesIO buffer and executes it in response.
+    pdf = buffer.getvalue()
+    buffer.close()
+    response.write(pdf)
     return response
